df5e2e97a6f3123592f3f3fc2fa0eb9df7a18f8b
nobu09 <11857481+nobu09@users.noreply.github.com>
2023-07-27 07:06:50 +0900

    npm startするとnpmエラーとなり起動できないので、お手本のgithubサンプルコードからpackage.jsonのバージョンを合わせたりコードを変えて動くようにした
    https://github.com/MoonHighway/learning-graphql

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { render } from 'react-dom'
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import ApolloClient, { gql } from 'apollo-boost';
 
 const client = new ApolloClient({ uri: 'http://localhost:4000/graphql' })
@@ -17,14 +16,10 @@ const query = gql`
 client.query({query})
   .then(({data}) => console.log('data', data))
   .catch(console.error)
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
 
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
