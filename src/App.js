3e26bac1c50fbd5d7ae9ab341674ae3b978a05a9
nobu09 <11857481+nobu09@users.noreply.github.com>
2023-07-31 07:14:38 +0900

    AppコンポーネントにROOT_QUERYの作成

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import logo from './logo.svg';
 import React from 'react';
-import './App.css';
+import User from './User';
+import { gql } from 'apollo-boost';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const ROOT_QUERY = gql`
+  query allUsers {
+    totalUsers
+    allUsers {
+      githubLogin
+      name
+      avatar
+    }
+  }
+`
 
+const App = () => <User />;
 export default App;
