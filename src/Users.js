cbb04ea36db36214fe14fe12c42db6eda953652a
nobu09 <11857481+nobu09@users.noreply.github.com>
2023-08-01 07:38:40 +0900

    refetchを使ってデータを再取得するようにする https://www.apollographql.com/docs/react/data/queries/#refetching

--- src/Users.js
+++ src/Users.js
@@ -4,15 +4,18 @@ import { ROOT_QUERY } from './App';
 
 const Users = () =>
   <Query query={ROOT_QUERY}>
-    { ({data, loading}) => loading ?
+    { ({data, loading, refetch}) => loading ?
       <p>loading users...</p> :
-      <UserList count={data.totalUsers} users={data.allUsers} />
+      <UserList count={data.totalUsers}
+        users={data.allUsers}
+        refetchUsers={refetch} />
     }
   </Query>
 
-const UserList = ({count, users}) =>
+const UserList = ({count, users, refetchUsers}) =>
   <div>
     <p>{count} Users</p>
+    <button onClick={() => refetchUsers()}>Refetch Users</button>
     <ul>
       {users.map(user =>
           <UserListItem key={user.githubLogin} name={user.name} avatar={user.avatar} />
