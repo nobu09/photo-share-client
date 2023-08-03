34a8c10f5740a6a5e947bab8d077ce60aab58e02
nobu09 <11857481+nobu09@users.noreply.github.com>
2023-08-03 11:40:53 +0900

    Mutationに、refetchQueriesのpropsを追加して、ミューテーションが完了した時にデータを再取得するようにした

--- src/Users.js
+++ src/Users.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Query } from 'react-apollo';
+import { Query, Mutation } from 'react-apollo';
 import { gql } from 'apollo-boost';
 import { ROOT_QUERY } from './App';
 
@@ -16,7 +16,16 @@ const Users = () =>
 const UserList = ({count, users, refetchUsers}) =>
   <div>
     <p>{count} Users</p>
+
     <button onClick={() => refetchUsers()}>Refetch Users</button>
+
+    <Mutation mutation={ADD_FAKE_USERS_MUTATION}
+      variables={{ count: 1 }}
+      refetchQueries={[{ query: ROOT_QUERY }]}
+    >
+      { addFakeUsers => <button onClick={addFakeUsers}>Add Fake Users</button> }
+    </Mutation>
+
     <ul>
       {users.map(user =>
           <UserListItem key={user.githubLogin} name={user.name} avatar={user.avatar} />
