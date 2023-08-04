cc83ac945a3293ed56ec5a9c8b4d139be0a52863
nobu09 <11857481+nobu09@users.noreply.github.com>
2023-08-04 18:49:34 +0900

    AuthorizedUserコンポーネントをクラスコンポーネントから関数コンポーネントに書き直した

--- src/AuthorizedUser.js
+++ src/AuthorizedUser.js
@@ -1,6 +1,30 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 
-class AuthorizedUser extends Component {
-  state = { signingIn: false }
+const AuthorizedUser = () => {
+  const [signingIn, setSigningIn] = useState(false);
+
+  useEffect(() => {
+    if (window.location.search.match(/code=/)) {
+      setSigningIn(true);
+      // コードを取得する
+      const code = window.location.search.replace("?code=", "");
+      alert(code);
+      // ユーザーをリダイレクトする
+      this.props.history.replace('/');
+    }
+  }, []);
+
+  const requestCode = () => {
+    const clientID = process.env.GITHUB_CLIENT_ID;
+    window.location = `https://github.com/login/oauth/authorize?client_id=${clientID}&scope=user`;
+  };
+
+  return (
+    <button onClick={requestCode} disabled={signingIn}>
+      Sign In with GitHub
+    </button>
+  )
 }
+
+export default withRouter(AuthorizedUser);
