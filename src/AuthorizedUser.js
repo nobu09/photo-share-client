e986c06b4897fe3f32c0c12f88cacc92cd626f65
nobu09 <11857481+nobu09@users.noreply.github.com>
2023-08-05 17:00:15 +0900

    React16.8.0以降でないとuseStateやuseEffectは使えなかった

--- src/AuthorizedUser.js
+++ src/AuthorizedUser.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const AuthorizedUser = () => {
+function AuthorizedUser() {
+  const [signingIn, setSigningIn] = useState(false); // eslint-disable-line
+
   const requestCode = () => {
     const clientID = "";
     window.location = `https://github.com/login/oauth/authorize?client_id=${clientID}&scope=user`;
   };
 
   return (
-    <button onClick={requestCode}>
+    <button onClick={requestCode} disabled={signingIn}>
       Sign In with GitHub
     </button>
   );
