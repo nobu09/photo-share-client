4f1c6ba4846770e7a3df9de37cbe7f84b4432341
nobu09 <11857481+nobu09@users.noreply.github.com>
2023-08-05 16:25:21 +0900

    Revert "ひと通り書いてみるとエラーになる"
    This reverts commit 949611961332f409c10aa47e23e663e2b99e1ba6.

--- src/AuthorizedUser.js
+++ src/AuthorizedUser.js
@@ -1,25 +1,13 @@
-import React, { useState, useEffect } from 'react';
-// import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import React from 'react';
 
 const AuthorizedUser = () => {
-  const [signingIn, setSigningIn] = useState(false);
-//   const history = useHistory();
-
-  useEffect(() => {
-    if (window.location.search.match(/code=/)) {
-      setSigningIn(true);
-      const code = window.location.search.replace("?code=", "");
-      alert(code);
-    }
-  });
-
   const requestCode = () => {
     const clientID = process.env.GITHUB_CLIENT_ID;
     window.location = `https://github.com/login/oauth/authorize?client_id=${clientID}&scope=user`;
   };
 
   return (
-    <button onClick={requestCode} disabled={signingIn}>
+    <button onClick={requestCode}>
       Sign In with GitHub
     </button>
   );
