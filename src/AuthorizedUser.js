50a50b76de5ada3557d1e42dcdcc2ee9e281bb6c
nobu09 <11857481+nobu09@users.noreply.github.com>
2023-08-05 17:16:53 +0900

    AuthorizedUserコンポーネントから、codeを使ってgithubAuthミューテーションを送る

--- src/AuthorizedUser.js
+++ src/AuthorizedUser.js
@@ -1,5 +1,14 @@
 import React, {Component} from 'react';
 import { withRouter } from 'react-router-dom';
+import { Mutation } from 'react-apollo';
+import { gql } from 'apollo-boost';
+import { ROOT_QUERY } from './App';
+
+const GITHUB_AUTH_MUTATION = gql`
+  mutation githubAuth($code:String!) {
+    githubAuth(code:$code) { token }
+  }
+`
 
 class AuthorizedUser extends Component {
   state = { signingIn: false };
@@ -13,6 +22,13 @@ class AuthorizedUser extends Component {
     }
   }
 
+  // ミューテーション完了時に呼び出される
+  authorizationComplete = (cache, { data }) => {
+    localStorage.setItem("token", data.githubAuth.token);
+    this.props.history.replace("/");
+    this.setState({ signingIn: false });
+  }
+
   requestCode() {
     const clientID = "";
     window.location = `https://github.com/login/oauth/authorize?client_id=${clientID}&scope=user`;
@@ -20,9 +36,19 @@ class AuthorizedUser extends Component {
 
   render() {
     return (
-      <button onClick={this.requestCode} disabled={this.state.signingIn}>
-        Sign In with GitHub
-      </button>
+      <Mutation mutation={GITHUB_AUTH_MUTATION} 
+        update={this.authorizationComplete}
+        refetchQueries={[{ query: ROOT_QUERY }]}>
+          {mutation => {
+            this.githubAuthMutation = mutation;
+            return (
+              <button onClick={this.requestCode} disabled={this.state.signingIn}>
+                Sign In with GitHub
+              </button>
+
+            )
+          }}
+      </Mutation>
     );
   }
 }
