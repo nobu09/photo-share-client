7300086c73dae92a590173cd49494f4a6de5b218
nobu09 <11857481+nobu09@users.noreply.github.com>
2023-08-06 15:52:15 +0900

    addFakeUsersで、レスポンスの追加されたFakeUserをキャッシュに書き戻す

--- src/Users.js
+++ src/Users.js
@@ -13,6 +13,13 @@ const Users = () =>
     }
   </Query>
 
+const updateUserCache = (cache, { data: { addFakeUsers } }) => {
+  let data = cache.readQuery({ query: ROOT_QUERY })
+  data.totalUsers += addFakeUsers.length;
+  data.allUsers = [...data.allUsers, ...addFakeUsers];
+  cache.writeQuery({ query: ROOT_QUERY, data })
+}
+
 const UserList = ({count, users, refetchUsers}) =>
   <div>
     <p>{count} Users</p>
@@ -21,7 +28,7 @@ const UserList = ({count, users, refetchUsers}) =>
 
     <Mutation mutation={ADD_FAKE_USERS_MUTATION}
       variables={{ count: 1 }}
-      refetchQueries={[{ query: ROOT_QUERY }]}
+      update={updateUserCache}
     >
       { addFakeUsers => <button onClick={addFakeUsers}>Add Fake Users</button> }
     </Mutation>
